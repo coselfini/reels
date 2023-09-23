72750db4b1215f3d4c7314f3ed7ab2117bea434e
Alex <86346171+coselfini@users.noreply.github.com>
2023-09-24 01:03:15 +0400

    Add files via upload

---
+++ Cards Glow Effect/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Светящийся эффект карточки | @alex__sleepwalker</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+    <main class="main flow">
+        <h1 class="main__heading">Тарифы</h1>
+        <div class="main__cards cards">
+            <div class="cards__inner">
+                <div class="cards__card card">
+                    <h2 class="card__heading">Базовый</h2>
+                    <p class="card__price">&#8381;999</p>
+                    <ul role="list" class="card__bullets flow">
+                        <li>Доступ ко всем стандартным функциям</li>
+                        <li>Техническая поддержка</li>
+                    </ul>
+                    <a href="#basic" class="card__cta cta">Начать</a>
+                </div>
+                <div class="cards__card card">
+                    <h2 class="card__heading">Продвинутый</h2>
+                    <p class="card__price">&#8381;2999</p>
+                    <ul role="list" class="card__bullets flow">
+                        <li>Расширенный набор функций и сервисов</li>
+                        <li>Приоритетная поддержка</li>
+                        <li>Полный доступ к дополнительным возможностям</li>
+                    </ul>
+                    <a href="#pro" class="card__cta cta">Начать</a>
+                </div>
+                <div class="cards__card card">
+                    <h2 class="card__heading">VIP "Эксклюзив"</h2>
+                    <p class="card__price">&#8381;8999</p>
+                    <ul role="list" class="card__bullets flow">
+                        <li>Премиальные функции и сервисы</li>
+                        <li>Высокий приоритет поддержки</li>
+                        <li>Индивидуальный подход к клиенту</li>
+                        <li>Ранний доступ к новым функциям</li>
+                    </ul>
+                    <a href="#ultimate" class="card__cta cta">Начать</a>
+                </div>
+            </div>
+            <div class="overlay cards__inner"></div>
+        </div>
+    </main>
+    <script src="/script.js" charset="utf-8"></script>
+</body>
+
+</html>
